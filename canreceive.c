a8a02a43b3c2d52c5b1e80a9a6534aff68540e07
Craig Peacock <38600596+craigpeacock@users.noreply.github.com>
2019-12-31 19:05:25 -0800

    First commit

---
+++ canreceive.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+int main(int argc, char **argv)
+{
+	int s, i; 
+	int nbytes;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+	struct can_frame frame;
+
+	printf("CAN Sockets Receive Demo\r\n");
+
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		perror("Socket");
+		return 1;
+	}
+
+	strcpy(ifr.ifr_name, "vcan0" );
+	ioctl(s, SIOCGIFINDEX, &ifr);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		perror("Bind");
+		return 1;
+	}
+
+	nbytes = read(s, &frame, sizeof(struct can_frame));
+
+ 	if (nbytes < 0) {
+		perror("Read");
+		return 1;
+	}
+
+	printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
+
+	for (i = 0; i < frame.can_dlc; i++)
+		printf("%02X ",frame.data[i]);
+
+	printf("\r\n");
+
+	if (close(s) < 0) {
+		perror("Close");
+		return 1;
+	}
+
+	return 0;
+}
